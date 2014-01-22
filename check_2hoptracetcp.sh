f909e69952c653bdfd8adaf3a31486fe6c7643ad
gouldchu <gouldchu@gmail.com>
2014-01-22 19:20:28 +0800

    check_2hoptracetcp
    Based on check_2hoptrace.sh. It trace between 2 hops with TCP SYN
    packets rather than UDP or ICMP ECHO packets.

---
+++ check_2hoptracetcp.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+PROG="/usr/bin/traceroute -n"
+GREP="/bin/egrep"
+HOST=$1
+FILE=/tmp/check_tracert-`date +%N`.txt
+START="$2"
+LAST="$3"
+
+if [ $# -lt 2 ]
+  then
+    echo "Usage: check_tracert [host] [ip we should route via] [last hop]"
+  exit 2
+fi
+
+$PROG $HOST > $FILE
+
+RESULT=`$GREP -c "($START|$LAST)" $FILE`
+
+case "$RESULT" in
+  0) echo "CRITICAL! Routing not matching!!!"
+     rm -f $FILE
+     echo $RESULT > /tmp/result
+     exit 2
+  ;;
+
+  1) echo "WARNING! One hop route not found!" 
+     rm -f $FILE
+     echo $RESULT > /tmp/result
+     exit 1
+  ;;
+  
+  2) echo "OK! Currently routing via $LAST" 
+     rm -f $FILE
+     echo $RESULT > /tmp/result
+     exit 0
+  ;;
+  
+  *) echo "UNKNOWN!"
+     rm -f $FILE
+     echo $RESULT > /tmp/result
+     exit 3
+esac
+
